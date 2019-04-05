3f6de352ed44e37173f782688de93d9e0e86444b
Alex <asz@xmail.net>
2019-04-05 19:46:58 +0300

    Курс java Prof ITVDN

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+
+    private int legs;
+
+    public Animal(int legs) {
+    }
+
+    public Animal() {
+    }
+
+    public void getLegrCount() {
+    }
+}
---
+++ src/Pet.java
@@ -0,0 +1,17 @@
+ public class Pet extends Animal {
+
+    private String name;
+
+    public Pet(int legs, String name) {
+    }
+
+    public Pet(String name) {
+    }
+
+    public Pet() {
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
